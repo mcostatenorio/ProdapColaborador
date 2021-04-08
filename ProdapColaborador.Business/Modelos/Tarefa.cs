dc1397ec6968349138577d51340c5d7a72f9a5b1
Milton Tenório <miltontenoriodacosta@outlook.com>
2021-04-08 01:10:14 -0300

    Inicio criação tela de tarefas

---
+++ ProdapColaborador.Business/Modelos/Tarefa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProdapColaborador.Business.Modelos
+{
+    public class Tarefa : EntitadeBase
+    {
+        public string Descricao { get; set; }
+
+        public int IdUsuarioResponsavel { get; set; }
+    }
+}
